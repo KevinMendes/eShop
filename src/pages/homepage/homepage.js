04b6e289a96b3632484bfa6d86e03c4635933e21
KevinMendes <mendes.kevin35@gmail.com>
2020-02-17 11:25:09 +0100

    Homepage + directory components + style

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,41 +1,13 @@
 import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.style.scss';
 
 
 const HomePage = () => (
     <div classname='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Chapeau</h1>
-                    <span className='subtitle'>Acheter</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Veste</h1>
-                    <span className='subtitle'>Acheter</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Sneakers</h1>
-                    <span className='subtitle'>Acheter</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Femmes</h1>
-                    <span className='subtitle'>Acheter</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>Hommes</h1>
-                    <span className='subtitle'>Acheter</span>
-                </div>
-            </div>
-        </div>
+        <Directory />
     </div>
 );
 
