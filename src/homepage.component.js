7877dfc7cee4c460621e43fd9977ad4b64fcc791
KevinMendes <mendes.kevin35@gmail.com>
2020-02-17 10:39:35 +0100

    directory without style

---
+++ src/homepage.component.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div classname='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Chapeau</h1>
+                    <span className='subtitle'>Acheter</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Veste</h1>
+                    <span className='subtitle'>Acheter</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Sneakers</h1>
+                    <span className='subtitle'>Acheter</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Femmes</h1>
+                    <span className='subtitle'>Acheter</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Hommes</h1>
+                    <span className='subtitle'>Acheter</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
